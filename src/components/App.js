15b4e04b489288061e5e6456fa06025ed7ea6dcc
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-01-21 17:22:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,29 @@
-import React, { useState } from "react";
-import MenuList from "./MenuList";
-import Categories from "./Categories";
-import { menuData } from "./data";
-import "../styles/App.css";
+import React, { useState } from 'react';
+import './../styles/App.css';
+import MenuList from './MenuList';
+import Categories from './Categories';
+import menu from './../data/data';
 
-const allCategories = ["All", ...new Set(menuData.map((item) => item.category))];
+const allCategories = ['all', ...new Set(menu.map(item => item.category))];
 
 const App = () => {
-  const [menu, setMenu] = useState(menuData);
+  const [items, setItems] = useState(menu);
   const [categories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if (category === "All") {
-      setMenu(menuData);
+    if (category === 'all') {
+      setItems(menu);
     } else {
-      setMenu(menuData.filter((item) => item.category === category));
+      const newItems = menu.filter(item => item.category === category);
+      setItems(newItems);
     }
   };
 
   return (
-    <div className="main">
+    <div className="app">
       <h1>Our Menu</h1>
       <Categories categories={categories} filterItems={filterItems} />
-      <MenuList menu={menu} />
+      <MenuList items={items} />
     </div>
   );
 };
