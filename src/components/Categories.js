b7184d8bcd80985821ae3d69c4a7f29e7378b34d
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-01-21 16:57:49 +0530

    Categories.js

---
+++ src/components/Categories.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="categories">
+      {categories.map((category, index) => (
+        <button key={index} onClick={() => filterItems(category)}>
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
