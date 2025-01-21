4db28e73321740503f64305b8f59ed31397ad5b6
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-01-21 17:21:32 +0530

    Update MenuList.js

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-const MenuList = ({ menu }) => {
+const MenuList = ({ items }) => {
   return (
-    <div className="menu-list">
-      {menu.map(({ id, title, price, img, desc }) => (
+    <div className="menu">
+      {items.map(({ id, title, category, price, img, desc }) => (
         <div key={id} className="menu-item">
           <img src={img} alt={title} />
-          <div className="item-info">
+          <div className="menu-info">
             <h3>{title} <span>${price}</span></h3>
             <p>{desc}</p>
           </div>
